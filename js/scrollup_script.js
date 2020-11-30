dc30862bbd6a904897b7f40b780b812a4ca7c077
supernagibator3000 <71119087+supernagibator3000@users.noreply.github.com>
2020-11-30 17:48:32 +0300

    Add files via upload

--- js/scrollup_script.js
+++ js/scrollup_script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
- 
     $(window).scroll(function(){
     if ($(this).scrollTop() > 100)
         $('.scrollup').fadeIn();
@@ -10,5 +9,5 @@ $(document).ready(function(){
     $('.scrollup').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 600);
     return false;
-    });    
+    });
 });
